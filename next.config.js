7dc869017b8f2f51d234ca0065a45f8ff50ee629
N41LL1W <willian.play.cel@gmail.com>
2026-05-14 09:40:32 -0300

    Fix: Error 404 no scrap v0.13

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const isStaticExport = process.env.CAPACITOR_BUILD === 'true';
 
 const nextConfig = {
   ...(isStaticExport ? { output: 'export' } : {}),
+    outputFileTracingIncludes: {
+    '/api/teste-semiurbano': ['./.cache/puppeteer/**/*', './.puppeteerrc.cjs'],
+    '/api/*': ['./.cache/puppeteer/**/*', './.puppeteerrc.cjs'],
+  },
 };
 
 module.exports = nextConfig;
